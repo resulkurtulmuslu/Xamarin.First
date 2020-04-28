361db9dd5c239a75327efef3cbdfecae303058dc
Resul Kurtulmuşlu <www.prometer.com.tr:5880>
2020-04-28 22:27:39 +0300

    Başlangıç

---
+++ Mobile.First.Solutions/Mobile.First/Mobile.First/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Mobile.First
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            sldr_Value.Value = 0.5;
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            lbl_Value.Text = string.Format("Değer {0:F2}", e.NewValue);
+        }
+
+        async private void btn_Next_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NextPage());
+        }
+    }
+}
---
+++ Mobile.First.Solutions/Mobile.First/Mobile.First/NextPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mobile.First
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NextPage : ContentPage
+    {
+        public NextPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Back_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+    }
+}
